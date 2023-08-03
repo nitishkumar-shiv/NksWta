dfc8fe31e2bcebd02fda50fed88a23f7d29540a2
nitishkumar-shiv <>
2023-08-04 00:09:27 +0530

    handled pop data

--- app/src/main/java/com/www/nkswta/FragmentHome.java
+++ app/src/main/java/com/www/nkswta/FragmentHome.java
@@ -9,27 +9,41 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.www.nkswta.db.TaskInfoDAO;
+import com.www.nkswta.db.TaskInformationDataBase;
+import com.www.nkswta.db.entity.TaskInfo;
+
+import java.util.ArrayList;
 
 
 public class FragmentHome extends Fragment {
 
     FragmentInteractionListener fragmentInteractionListener;
     RecyclerView recyclerViewFragmentHome;
-    TextView user_name;
+    TextView user_name,editableModalTitleText,editableModalCardPercentage,editableCardLocationText;
+    EditText editableModalEditTitleText,editableModalPriorityEditText,editableModalTypeEditText;
+    Button editableCardConfirmButton;
+    SeekBar editableCardSeekBar;
     ModalClass[] MyListData;
     CustomAdaptor adaptor;
     SharedPreferences sharedPreferences;
     String userName;
-
+    String taskID, taskTitle, priority, taskType, location;
+    int progress;
+    TaskInformationDataBase taskInformationDataBase;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -126,7 +140,7 @@ public class FragmentHome extends Fragment {
                     // Handle the click event for each card item here
                 Log.d("itemClick", "onItemClick: ");
                 Toast.makeText(getActivity(), "Clicked on item " + (position + 1), Toast.LENGTH_SHORT).show();
-                showAlertDialogButtonClicked();
+                showAlertDialogButtonClicked(MyListData[position]);
             }
         });
         recyclerViewFragmentHome.setHasFixedSize(true);
@@ -135,13 +149,59 @@ public class FragmentHome extends Fragment {
 
         return view;
     }
-    public void showAlertDialogButtonClicked() {
+    public void showAlertDialogButtonClicked(ModalClass myListDatum) {
         // Create an alert builder
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
         final View customLayout = getLayoutInflater().inflate(R.layout.card_editable_modal, null);
         builder.setView(customLayout);
         AlertDialog dialog = builder.create();
+
+        editableModalTitleText = customLayout.findViewById(R.id.editableModalTitleText);
+        editableModalEditTitleText = customLayout.findViewById(R.id.editableModalEditTitleText);
+        editableModalPriorityEditText = customLayout.findViewById(R.id.editableModalPriorityEditText);
+        editableModalTypeEditText = customLayout.findViewById(R.id.editableModalTypeEditText);
+        editableCardSeekBar = customLayout.findViewById(R.id.editableCardSeekBar);
+        editableModalCardPercentage = customLayout.findViewById(R.id.editableModalCardPercentage);
+        editableCardLocationText = customLayout.findViewById(R.id.editableCardLocationText);
+        editableCardConfirmButton = customLayout.findViewById(R.id.editableCardConfirmButton);
+
+        editableModalTitleText.setText(myListDatum.getTaskID());
+        editableModalEditTitleText.setText(myListDatum.getTaskTitle());
+        editableModalPriorityEditText.setText(myListDatum.getPriority());
+        editableModalTypeEditText.setText(myListDatum.getTaskType());
+        editableModalCardPercentage.setText(Integer.toString(myListDatum.getProgress())+"%");
+        editableCardSeekBar.setProgress(myListDatum.getProgress());
+
+        editableCardSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                editableModalCardPercentage.setText(Integer.toString(i)+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        editableCardConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                taskID = editableModalTitleText.getText().toString();
+                taskTitle = editableModalEditTitleText.getText().toString();
+                priority = editableModalPriorityEditText.getText().toString();
+                taskType = editableModalTypeEditText.getText().toString();
+                progress = editableCardSeekBar.getProgress();
+                location = editableCardLocationText.getText().toString();
+                Log.d("info", "onClick: "+taskID+taskTitle+priority+taskType+progress+location);
+                dialog.dismiss();
+            }
+        });
 
         dialog.show();
     }
